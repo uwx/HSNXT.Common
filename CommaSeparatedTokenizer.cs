4016b1cd23b0416ad9a927e97cb2f16ca9588bdb
uwx <chris331hansen@outlook.com>
2019-02-11 05:45:25 -0200

    Update CommaSeparatedTokenizer.cs

--- CommaSeparatedTokenizer.cs
+++ CommaSeparatedTokenizer.cs
@@ -15,18 +15,30 @@ namespace HSNXT.Common
         {
             IEnumerable<string> TokenizeInternal()
             {
-                var tokenizer = StringTokenizer.Tokenize(str);
-
+                var tokenizer = StringTokenizer.Tokenize(str, commentSymbol);
                 foreach (var token in tokenizer)
                 {
-                    // don't split if the token only contains a comma with nothing after it
-                    if (token[token.Length - 1] == ',') continue;
-                    
-                    foreach (var entry in token.Split(','))
+                    // don't split if the token only contains an ending comma
+                    if (token.IndexOf(',') == token.Length - 1)
+                    {
+                        yield return token.Substring(0, token.Length - 1);
+                        continue;
+                    }
+
+                    var newToken = token;
+
+                    // remove the ending comma (if the token contains a separator comma AND and ending comma)
+                    if (newToken.EndsWith(','))
+                    {
+                        newToken = newToken.Substring(0, token.Length - 1);
+                    }
+
+                    foreach (var entry in newToken.Split(','))
                     {
                         yield return entry;
                     }
                 }
+
             }
 
             return new CommaSeparatedTokenizer(TokenizeInternal());
